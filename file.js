5777be52e0e4d5bd9680bc33503d53c0eff29ae9
ogneyar <ogneyar-ne@ya.ru>
2020-10-31 10:19:23 +0300

    hh

--- file.js
+++ file.js
@@ -3,5 +3,6 @@ var fs = require('fs');
 fs.mkdir('temp', function() {
     fs.writeFile('./temp/temp.txt', 'Hell Work', function() {
         console.log('Всё работает');
+        console.log('Создал папку и файл');
     });
 });
