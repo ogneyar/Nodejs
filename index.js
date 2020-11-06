2c4a092eb4bc2a966da4eef770430b7699906eb0
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-11-06 20:59:32 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express();
 app.use('/public', express.static('public'));
 
-app.set('view engine', 'ejs')
+app.set('views', 'views');
+
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
     res.render('home');
@@ -21,5 +23,4 @@ app.get('/testy', function(req, res){
     res.sendFile(__dirname + '/web/index.html');
 });
 
-app.listen(8000);
-console.log('Starting my server on NodeJS: http://127.0.0.1:8000');
+app.listen(8000, () => console.log('Starting my server on NodeJS: http://127.0.0.1:8000'));
