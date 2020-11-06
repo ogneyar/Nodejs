93d42c2a1662fc8ba761ef3e3ee6c7f3b7018bd3
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-11-06 21:36:20 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const path = require('path')
 const PORT = process.env.PORT || 8000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
+  .use(express.static('public'))
+  .set('views', 'views')
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('home'))
   .get('/test', (req, res) => res.render('test', {test: 'test text'}))
